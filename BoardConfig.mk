7e66ab88b8aacf902715116b2ab10f7541e9118a
bonuzzz <bonuzzz@gmail.com>
2018-12-06 22:10:42 +0100

    Create lineage-15.1 branch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,22 +1,20 @@
-include device/samsung/gts2-common/BoardConfigCommon.mk
-
 LOCAL_PATH := device/samsung/gts28ltexx
 
 # Include path
-TARGET_SPECIFIC_HEADER_PATH += $(LOCAL_PATH)/include
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
 # Kernel
-TARGET_KERNEL_CONFIG := cm_exynos5433-gts28lte_defconfig
+TARGET_KERNEL_CONFIG := lineage_gts28ltexx_defconfig
+
+# Properties
+TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
 
 # RIL
 BOARD_MODEM_TYPE := ss333
 BOARD_PROVIDES_LIBRIL := true
 
-# RIL.java overwrite
-BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril
-
 # Recovery
 TARGET_OTA_ASSERT_DEVICE := gts28lte,gts28ltexx
 
-# SELinux
-BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
+# Inherit common board flags
+include device/samsung/gts2-common/BoardConfigCommon.mk
--- device.mk
+++ device.mk
@@ -1,26 +1,43 @@
 LOCAL_PATH := device/samsung/gts28ltexx
 
-###########################################################
-### RAMDISK
-###########################################################
-
+# Ramdisk
 PRODUCT_PACKAGES += \
+	ueventd.universal5433.rc \
     init.baseband.rc
 
-###########################################################
-### AUDIO
-###########################################################
-
+# Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
-    $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
+    $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_0.xml
 
-###########################################################
-### BLUETOOTH
-###########################################################
-
+# Bluetooth
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+    $(LOCAL_PATH)/bluetooth/bt_vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/bt_vendor.conf
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+	device/samsung/gts28ltexx/overlay	
+	
+# Radio
+PRODUCT_PACKAGES += \
+    libxml2 \
+    libprotobuf-cpp-full \
+    android.hardware.radio@1.0 \
+    android.hardware.radio.deprecated@1.0
+
+PRODUCT_PACKAGES += \
+    rild \
+    libril \
+    libsecril-client \
+    libsecril-client-sap \
+    modemloader
+
+PRODUCT_PACKAGES += \
+    SamsungServiceMode	
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml
+	
 # call the proprietary setup
-$(call inherit-product-if-exists, vendor/samsung/gts28ltexx/gts28ltexx-vendor.mk)
+$(call inherit-product, vendor/samsung/gts28ltexx/gts28ltexx-vendor.mk)
