9d712d18763d920124fa7a32ce47b73f207e25b5
ananjaser1211 <ananjaser@gmail.com>
2020-12-17 01:01:13 +0100

    gts28ltexx: Drop module eng tags

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= rild.rc
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= BOOT
 LOCAL_SRC_FILES		:= etc/rild.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
@@ -10,7 +10,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.baseband.rc
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= BOOT
 LOCAL_SRC_FILES		:= etc/init.baseband.rc
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -18,7 +18,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= ueventd.universal5433.rc
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= BOOT
 LOCAL_SRC_FILES		:= etc/ueventd.universal5433.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
