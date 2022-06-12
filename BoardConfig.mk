fcb0817c352e7aa4ebd07cfc6d0b1c73c7dc33ad
metterschlg <106471001+metterschlg@users.noreply.github.com>
2022-06-12 19:19:18 +0200

    Ensure that device-specific manifest contents get loaded.
    By adding the device-specific (gts28ltexx) manifest contents _after_
    the generic one (gts2-common), we ensure that both contents are combined.
    In the previous code, the generic manifest overwrote the device-specific one,
    thus the RIL-specific additions got lost.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,8 +1,5 @@
 LOCAL_PATH := device/samsung/gts28ltexx
 
-# Hidl
-DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/manifest.xml
-
 # Include path
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
@@ -21,3 +18,9 @@ TARGET_OTA_ASSERT_DEVICE := gts28lte,gts28ltexx
 
 # Inherit common board flags
 include device/samsung/gts2-common/BoardConfigCommon.mk
+
+# Add RIL-specific HIDL manifest _after_ the common one.
+#   Note that LOCAL_PATH gets overwritten by the common
+#   board config so we need to explicitly set here again.
+LOCAL_PATH := device/samsung/gts28ltexx
+DEVICE_MANIFEST_FILE += $(LOCAL_PATH)/manifest.xml
