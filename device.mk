3126e27dd068824e2c6c2dfe22e03dbb8cf97851
metterschlg <106471001+metterschlg@users.noreply.github.com>
2022-12-23 12:55:07 +0100

    Remove stale package to fix Lineage 20.0 build.

--- device.mk
+++ device.mk
@@ -39,8 +39,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
 
-PRODUCT_PACKAGES += \
-    SamsungServiceMode	
+#PRODUCT_PACKAGES += \
+#    SamsungServiceMode
 
 # RIL-specific prereq libraries
 PRODUCT_COPY_FILES += \
